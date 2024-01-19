0a47426b627cc51e5ee2f0fc510690757a404d6c
VivianaS14 <vasorianp@gmail.com>
2024-01-18 20:05:28 -0500

    Server

--- http.js
+++ http.js
@@ -3,8 +3,19 @@ const http = require('node:http')
 const desiredPort = process.env.PORT ?? 1234
 
 const server = http.createServer((req, res) => {
-  console.log('Request received ', req.method)
-  res.end('Hola mundo')
+  if (req.url === '/') {
+    res.statusCode = 200 // OK
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    res.end('Bienvenido a la página de inicio')
+  } else if (req.url === '/contacto') {
+    res.statusCode = 200 // OK
+    res.setHeader('Content-type', 'application/json')
+    res.end(JSON.stringify({ contacto: 'Sofia Alejandra' }))
+  } else {
+    res.statusCode = 404 // Not Found
+    res.setHeader('Content-type', 'text/html')
+    res.end('<h1>404: Not Found</h1>')
+  }
 })
 
 server.listen(desiredPort, () => {
